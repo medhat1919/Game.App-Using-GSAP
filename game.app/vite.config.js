fe6329f302438a55c9d78326b94f03552d4901dc
medhat <medhatmoris90@gmail.com>
2025-10-01 21:56:23 +0300

    Fix

--- game.app/vite.config.js
+++ game.app/vite.config.js
@@ -7,4 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
+  base: process.env.Vite_BASE.PATH || '/Game.App-Using-GSAP',
 })
